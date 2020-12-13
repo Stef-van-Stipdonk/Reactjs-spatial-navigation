0b10da2fc480ece0f8b19a3f40e8dd361f21a49f
Vash <mrbats1234@gmail.com>
2020-12-13 14:59:34 +0100

    Added functionality to get rectangle position based on center point

--- reactjs-spatial-navigation/src/Components/SpatialNav.js
+++ reactjs-spatial-navigation/src/Components/SpatialNav.js
@@ -7,23 +7,45 @@ let KEYMAP = {
     "37": "left",
     "38": "up",
     "39": "right",
-    "40": "down"
+    "40": "down",
+    "13": "enter"
 }
 
 let _ready = false;
 
 // let direction = KEYMAP[keyEvent.keyCode];
 
+function getRectPosition(e){
+    var currRect = e.getBoundingClientRect();
+    var rect = {
+        left: currRect.left,
+        top: currRect.top,
+        right: currRect.right,
+        bottom: currRect.bottom,
+        width: currRect.width,
+        height: currRect.height
+    };
+    rect.element = e;
+    rect.center = {
+        x: rect.left + Math.floor(rect.width / 2),
+        y: rect.top + Math.floor(rect.height / 2)
+    }
+    rect.center.left = rect.center.right = rect.center.x;
+    rect.center.top = rect.center.bottom = rect.center.y;
+
+    console.log(rect);
+}
+
 function onKeyDown(e) {
-    console.log(KEYMAP[e.keyCode]);
+    console.log(`${KEYMAP[e.keyCode]} - ${e.keyCode}`);
 }
 
 function onKeyUp(e) {
-    console.log(KEYMAP[e.keyCode]);
+    console.log(`${KEYMAP[e.keyCode]} - ${e.keyCode}`);
 }
 
-function onFocus(e){
-    console.log(e);
+function onFocus(){
+    getRectPosition(document.activeElement);
 }
 
 let SpatialNav = {
@@ -33,7 +55,7 @@ let SpatialNav = {
                 GlobalConfig = config;
             };
             window.addEventListener("keydown", onKeyDown);
-            window.addEventListener("keyup", onKeyUp);
+            // window.addEventListener("keyup", onKeyUp);
             window.addEventListener("focus", onFocus, true);
             _ready = true;
 
@@ -43,7 +65,7 @@ let SpatialNav = {
     
     terminate: function(){
         window.removeEventListener("keydown", onKeyDown);
-        window.removeEventListener("keyup", onKeyUp);
+        // window.removeEventListener("keyup", onKeyUp);
         window.removeEventListener("focus", onFocus, true);
         _ready = false;
         console.log("Navigation Terminated")
